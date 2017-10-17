4d57e33bb8918b8a01e3fd840adec98adc8b10e3
goodgoof <anjalidsharma8@gmail.com>
2017-10-17 16:18:47 -0400

    last commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.getElementById('facebook').addEventListener('click', function(event) {
   var facebook = event.target.textContent
   if (facebook === 'facebook') {
-  event.target.textContent = 'abcd'
-} else if (facebook === 'abcd') {
+  event.target.textContent = 'Anjali'
+} else if (facebook === 'Anjali') {
   event.target.textContent = 'facebook'
 }
 });
